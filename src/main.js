879ccebde908dc3778b084d2e2d3e8a365b1ccfb
Tragislav Work PC <s.dykomenko@gmail.com>
2021-01-05 13:44:02 +0300

    demo JSON

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
 "use strict";
+
+let demojsonstringify = {
+    name: "Stanislav",
+    surname: "Dykomenko",
+    age: 27,
+    birthday: {
+        day: 19,
+        month: "July",
+        year: 2000
+    }
+}
+
+let demojsonparse = '{"name":"Stanislav","surname":"Dykomenko","age":27,"birthday":{"day":19,"month":"July","year":2000}}';
+
+console.log(JSON.stringify(demojsonstringify));
+console.log("_______________");
+console.log(JSON.parse(demojsonparse));
