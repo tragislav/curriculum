5eac14bf4a8e61649df7f4af36072ff10d38f4b6
Tragislav Work PC <s.dykomenko@gmail.com>
2021-01-05 13:58:55 +0300

    add file

--- src/main.js
+++ src/main.js
@@ -9,10 +9,19 @@ let demojsonstringify = {
         month: "July",
         year: 2000
     }
-}
+};
 
 let demojsonparse = '{"name":"Stanislav","surname":"Dykomenko","age":27,"birthday":{"day":19,"month":"July","year":2000}}';
 
 console.log(JSON.stringify(demojsonstringify));
 console.log("_______________");
 console.log(JSON.parse(demojsonparse));
+
+$(document).on('click', '#btn', function () {
+    var formData = new FormData();
+    formData.append("myFile", document.getElementById("file").files[0], 'chris1.jpg');
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "index.html");
+    xhr.send(formData);
+});
